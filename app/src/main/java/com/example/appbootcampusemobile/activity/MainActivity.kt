47ba1676f1e1dc69ff1b32fd9803ab198ba1454a
Priscilla Souza <priscillasilva-24@hotmail.com>
2022-06-17 15:22:28 -0300

    criando bottom navigation

---
+++ app/src/main/java/com/example/appbootcampusemobile/activity/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.appbootcampusemobile.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.example.appbootcampusemobile.R
+import com.example.appbootcampusemobile.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigation: BottomNavigationView
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setNavigationController()
+    }
+
+    private fun setNavigationController() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        bottomNavigation = findViewById(R.id.bottomNavigation)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.registerAnimalFragment,
+                R.id.favoritesFragment
+            )
+        )
+        bottomNavigation.setupWithNavController(navController)
+    }
+}
