c1fe94befcb236510d3e9ad58d7f9cbf04e5caa5
Priscilla Souza <priscillasilva-24@hotmail.com>
2022-06-23 00:26:02 -0300

    ajustando layouts

--- app/src/main/java/com/example/appbootcampusemobile/presentation/activity/SplashActivity.kt
+++ app/src/main/java/com/example/appbootcampusemobile/presentation/activity/SplashActivity.kt
@@ -1,6 +1,5 @@
-package com.example.appbootcampusemobile.activity
+package com.example.appbootcampusemobile.presentation.activity
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
